92c5c28fcd743dfbb0ff4ab352e481703d19da24
adem el abidi <elabidi.adem@hotmail.fr>
2021-01-23 02:28:37 +0100

    Seed : Créations des 7 jours du model Day

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# require 'faker'
+# require "open-uri"
+require 'date'
+
+puts "Cleaning database"
+
+Day.destroy_all
+
+puts "Create 7 Days"
+
+sunday = Day.create(name: "Monday")
+tuesday = Day.create(name: "Tuesday")
+wednesday = Day.create(name: "Wednesday")
+thursday = Day.create(name: "Thursday")
+friday = Day.create(name: "Friday")
+saturday = Day.create(name: "Saturday")
+sunday = Day.create(name: "Sunday")
+
+puts "Done"
