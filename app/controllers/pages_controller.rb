a1ba68873e0d3efbaaf07445c7879470551528e7
adem el abidi <elabidi.adem@hotmail.fr>
2021-01-23 02:51:28 +0100

    Seed : Créations des 2 rooms(salle) du model Room

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
     @days = Day.all
+    @rooms = Room.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,14 @@
 # require "open-uri"
 require 'date'
 
-puts "Cleaning database"
+puts "Cleaning database..."
 
 Day.destroy_all
+Room.destroy_all
 
-puts "Create 7 Days"
+puts "Cleaning database => OK"
+
+puts "Create 7 Days..."
 
 sunday = Day.create(name: "Monday")
 tuesday = Day.create(name: "Tuesday")
@@ -24,5 +27,12 @@ thursday = Day.create(name: "Thursday")
 friday = Day.create(name: "Friday")
 saturday = Day.create(name: "Saturday")
 sunday = Day.create(name: "Sunday")
+
+puts "Done"
+
+puts "Create 2 Rooms..."
+
+homme = Room.create(name: "Homme", capacity_room: 100)
+femme = Room.create(name: "Femme", capacity_room: 40)
 
 puts "Done"
