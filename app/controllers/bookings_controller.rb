948690d48aa59673d121835220963842502975ed
adem el abidi <elabidi.adem@hotmail.fr>
2021-02-14 02:18:48 +0100

    Def capacity_prayer_reset => Ok, testé uniquement en console de test

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -33,5 +33,21 @@ class BookingsController < ApplicationController
       render "new"
     end
   end
-  
+
+
+  def capacity_prayer_reset
+    yesterday = Date.today - 1
+    yesterday_name = yesterday.strftime("%A")
+    prayers_yesterday_homme = Prayer.all.where(room_id: Room.all.find_by_name('Homme'), day_id: Day.all.find_by_name(yesterday_name))
+    prayers_yesterday_femme = Prayer.all.where(room_id: Room.all.find_by_name('Femme'), day_id: Day.all.find_by_name(yesterday_name))
+
+    prayers_yesterday_homme.each do |prayer|
+      prayer.capacity_prayer = Room.all.find_by_name('Homme').capacity_room
+    end
+
+    prayers_yesterday_femme.each do |prayer|
+      prayer.capacity_prayer = Room.all.find_by_name('Femme').capacity_room
+    end
+  end
+
 end
